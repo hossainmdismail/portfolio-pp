2afbc31dc1b0f9e211693d52416c46da49cc5d13
Esmail Khalifa <120841700+hossainmdismail@users.noreply.github.com>
2025-04-20 18:20:56 +0600

    Reapply "updt wout sitmeap"
    This reverts commit f21aafceac70ccdd87152f705d215749d6ae2f66.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class HomeController extends Controller
 {
     public function index()
     {
+
+        SEOTools::setDescription('This is the portfolio of a Laravel developer focused on building modern, clean web apps.');
+        SEOTools::opengraph()->setUrl(url()->current());
+        SEOTools::setCanonical(url()->current());
+
         $project = Project::where('category_status', 'project')->latest()->get()->take(6);
         $casestudy = Project::where('category_status', 'casestudy')->latest()->get()->take(6);
         return view('themes.portfolio.index', [
